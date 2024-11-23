278dbfd9a2d51119b0141db70ad651e966388e82
milkymilky0116 <sjlee990129@gmail.com>
2024-11-23 20:07:56 +0900

    chore: update file

--- src/main/java/_oormthon/univ/flakeide/backend/auth/api/KakaoLoginController.java
+++ src/main/java/_oormthon/univ/flakeide/backend/auth/api/KakaoLoginController.java
@@ -5,13 +5,7 @@ import _oormthon.univ.flakeide.backend.auth.domain.User;
 import _oormthon.univ.flakeide.backend.auth.service.KakaoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
@@ -24,6 +18,7 @@ public class KakaoLoginController {
         this.kakaoService = kakaoService;
     }
 
+    @GetMapping("")
     @GetMapping("/oauth/callback/kakao")
     public @ResponseBody Token kakaoLogin(@RequestParam("code") String code) {
         String kakaoAccessToken = kakaoService.getAccessToken(code);
