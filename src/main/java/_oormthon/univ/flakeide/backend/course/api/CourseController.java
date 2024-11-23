fe1bc0d6ececcebf971075961c49d50ade07751c
ckr8305 <ckr8305@naver.com>
2024-11-23 21:17:21 +0900

    chore: update swagger document (#14)

--- src/main/java/_oormthon/univ/flakeide/backend/course/api/CourseController.java
+++ src/main/java/_oormthon/univ/flakeide/backend/course/api/CourseController.java
@@ -4,7 +4,13 @@ import _oormthon.univ.flakeide.backend.auth.api.dto.ListUserResDto;
 import _oormthon.univ.flakeide.backend.course.api.dto.CourseResDto;
 import _oormthon.univ.flakeide.backend.course.domain.Course;
 import _oormthon.univ.flakeide.backend.course.service.CourseService;
+import _oormthon.univ.flakeide.backend.global.exception.CustomException;
 import _oormthon.univ.flakeide.backend.global.util.user.snowPine.AccessSnowPineToken;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,12 +29,25 @@ public class CourseController {
     }
 
     @GetMapping("{courseId}")
+    @Operation(summary = "수업 상세 조회")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "성공적으로 수업 상세 조회함", content = @Content(mediaType = "application/json", schema = @Schema(implementation = CourseResDto.class))),
+        @ApiResponse(responseCode = "404", description = "수업을 찾지 못함", content = @Content(mediaType = "application/json", schema = @Schema(implementation = CustomException.class))),
+        @ApiResponse(responseCode = "500", description = "서버 에러", content = @Content(mediaType = "application/json", schema = @Schema(implementation = CustomException.class)))
+    })
     public ResponseEntity<CourseResDto> getCourseDetail(@PathVariable("courseId") long courseId) {
         return ResponseEntity.ok(courseService.getCourseDetail(courseId));
     }
 
     @GetMapping("{courseId}/snowflake")
     @AccessSnowPineToken
+    @Operation(summary = "수업에 참여하는 눈송이(snowflake) 조회")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "성공적으로 수업에 참여한 눈송이(snowflake) 조회함", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ListUserResDto.class))),
+        @ApiResponse(responseCode = "401", description = "인증 실패", content = @Content(mediaType = "application/json", schema = @Schema(implementation = CustomException.class))),
+        @ApiResponse(responseCode = "404", description = "수업을 찾지 못함", content = @Content(mediaType = "application/json", schema = @Schema(implementation = CustomException.class))),
+        @ApiResponse(responseCode = "500", description = "서버 에러", content = @Content(mediaType = "application/json", schema = @Schema(implementation = CustomException.class)))
+    })
     public ResponseEntity<ListUserResDto> getUsersOfCourse(@RequestHeader("Authorization") String authorizationHeader, @PathVariable("courseId") long curseId) {
         return ResponseEntity.ok(courseService.getUsersOfCourse(authorizationHeader, curseId));
     }
--- src/main/java/_oormthon/univ/flakeide/backend/snowPine/controller/SnowPineController.java
+++ src/main/java/_oormthon/univ/flakeide/backend/snowPine/controller/SnowPineController.java
@@ -2,9 +2,15 @@ package _oormthon.univ.flakeide.backend.snowPine.controller;
 
 import _oormthon.univ.flakeide.backend.auth.api.dto.ListUserResDto;
 import _oormthon.univ.flakeide.backend.course.api.dto.ListCourseResDto;
+import _oormthon.univ.flakeide.backend.global.exception.CustomException;
 import _oormthon.univ.flakeide.backend.global.util.user.snowPine.AccessSnowPineId;
 import _oormthon.univ.flakeide.backend.global.util.user.snowPine.AccessSnowPineToken;
 import _oormthon.univ.flakeide.backend.snowPine.service.SnowPineService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,6 +30,12 @@ public class SnowPineController {
 
     @GetMapping("/{snowpineId}/course")
     @AccessSnowPineId
+    @Operation(summary = "눈솔(snowPine)의 수업 조회")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "성공적으로 눈솔(snowPine)의 수업 조회함", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ListCourseResDto.class))),
+        @ApiResponse(responseCode = "404", description = "눈솔(snowPine)을 찾지 못함", content = @Content(mediaType = "application/json", schema = @Schema(implementation = CustomException.class))),
+        @ApiResponse(responseCode = "500", description = "서버 에러", content = @Content(mediaType = "application/json", schema = @Schema(implementation = CustomException.class)))
+    })
     public ResponseEntity<ListCourseResDto> getCourseOfSnowPine(@PathVariable("snowpineId") long snowPineId) {
         return ResponseEntity.ok(snowPineService.getCourseOfSnowPine(snowPineId));
     }
